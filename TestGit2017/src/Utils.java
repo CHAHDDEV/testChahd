0fa79108b9b205c85dcaa0125f07e2c9ad658f66
chahddev <ghnimi.chahd@gmail.com>
2017-04-16 10:11:24 -0400

    Commit chahd - add messages

--- TestGit2017/src/Utils.java
+++ TestGit2017/src/Utils.java
@@ -3,4 +3,7 @@ public class Utils {
 	public static void Messages(){
 		System.out.println("Hello le monde de git");
 	}
+	public static void MessagesChahd(){
+		System.out.println("Hello le monde de git");
+	}
 }
